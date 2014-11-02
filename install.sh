401aa29b2fbf6713ae2233540bf6f371b661b03e
Brayden Streeter <bstreete@email.arizona.edu>
2014-11-02 12:56:35 -0700

    Updated the install script to double check that the applications aren't already installed somewhere else in the path. Doesn't quit when errors are encountered.

--- install.sh
+++ install.sh
@@ -5,37 +5,42 @@
 
 if [ $? -ne 0 ] ; then 
 
-	echo "Installing CCTools..."
+	work_queue_status -v &> /dev/null 
+	
+	if [ $? -ne 0 ] ; then 
+		echo "Installing CCTools..."
 
-	# Download the source files
-	cd 
-	wget "http://ccl.cse.nd.edu/software/files/cctools-4.2.2-source.tar.gz"
-
-	# If the download completed successfully
-	if [ $? -eq 0 ] ; then
-		
-		# Extract
-		tar xzf cctools-4.2.2-source.tar.gz
-		cd cctools-4.2.2-source
-
-		echo "Configuring CCTools...."
-		./configure > /dev/null
-		make > /dev/null
-
-		echo "Installing to ~/cctools/bin...."
-		make install > /dev/null
-
+		# Download the source files
 		cd 
-		echo "Finished installing CCTools ~/cctools/bin."
+		wget "http://ccl.cse.nd.edu/software/files/cctools-4.2.2-source.tar.gz"
 
-	else 
-		echo "Failed to download and install CCTools from Notre Dame." 
+		# If the download completed successfully
+		if [ $? -eq 0 ] ; then
+		
+			# Extract
+			tar xzf cctools-4.2.2-source.tar.gz
+			cd cctools-4.2.2-source
+
+			echo "Configuring CCTools...."
+			./configure > /dev/null
+			make > /dev/null
+
+			echo "Installing to ~/cctools/bin...."
+			make install > /dev/null
+
+			cd 
+			echo "Finished installing CCTools ~/cctools/bin."
+
+		else 
+			echo "Failed to download and install CCTools from Notre Dame." 
+		fi
+	
+	else
+		echo "CCTools detected."
 	fi
 
 else
-
 	echo "CCTools detected."
-
 fi
 
 # Check for gdal packages
@@ -43,30 +48,38 @@ fi
 
 if [ $? -ne 0 ] ; then
 
-	echo "Installing GDAL...."
+	gdal-config --version &> /dev/null 
+	
+	if [ $? -ne 0 ] ; then 
 
-	# Download source files
-	cd 
-	wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz
+		echo "Installing GDAL...."
 
-	if [ $? -eq 0 ] ; then
-		# Extract files
-		tar -zxf gdal-1.11.1.tar.gz
-		cd gdal-1.11.1
+		# Download source files
+		cd 
+		wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz
 
-		echo "Configuring GDAL...."
-		echo "Be patient, this takes a couple of minutes."
-		./configure --prefix=$HOME > /dev/null
-		make > /dev/null
+		if [ $? -eq 0 ] ; then
+			# Extract files
+			tar -zxf gdal-1.11.1.tar.gz
+			cd gdal-1.11.1
 
-		echo "Installing GDAL in ~/bin...."
-		make install > /dev/null
-		cd $HOME
+			echo "Configuring GDAL...."
+			echo "Be patient, this takes a couple of minutes."
+			./configure --prefix=$HOME > /dev/null
+			make > /dev/null
 
-		echo "Finished installing GDAL to ~/bin."
+			echo "Installing GDAL in ~/bin...."
+			make install > /dev/null
+			cd $HOME
 
+			echo "Finished installing GDAL to ~/bin."
+
+		else
+			echo "Failed to download GDAL from OSGeo.org."
+		fi
+	
 	else
-		echo "Failed to download GDAL from OSGeo.org."
+		echo "GDAL detected."
 	fi
 
 else
@@ -76,22 +89,29 @@ fi
 # iCommands
 ~/icommands/ils -v &> /dev/null
 
-if [ $? -eq 0 ] ; then 
+if [ $? -ne 0 ] ; then 
 
-	echo "Installing iCommands...."
-
-	cd $HOME
-	wget http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2
-	
+	ils -v &> /dev/null
 	if [ $? -ne 0 ] ; then
-		echo "Extracting iCommands...."
-		tar -jxf icommands.x86_64.tar.bz2
 
-		echo "Finished installing iCommands to ~/icommands."
+		echo "Installing iCommands...."
+
 		cd $HOME
+		wget http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2
+	
+		if [ $? -ne 0 ] ; then
+			echo "Extracting iCommands...."
+			tar -jxf icommands.x86_64.tar.bz2
+
+			echo "Finished installing iCommands to ~/icommands."
+			cd $HOME
+
+		else
+			echo "Unable to download iCommands from iPlant."
+		fi
 
 	else
-		echo "Unable to download iCommands from iPlant."
+		echo "iCommands detected."
 	fi
 
 else
@@ -101,7 +121,6 @@ fi
 # Check for FFTW
 test -f ~/lib/libfftw3.la &> /dev/null
 # Installs into ~/bin and ~/include
-
 if [ $? -ne 0 ] ; then 
 
 	echo "Installing FFTW...."
@@ -171,31 +190,39 @@ fi
 ~/bin/grass64 --version >& /dev/null
 
 if [ $? -ne 0 ] ; then 
-	echo "Installing Grass...." 
 
-	cd $HOME
-	wget http://grass.osgeo.org/grass64/source/grass-6.4.4.tar.gz
-	
-	if [ $? -eq 0 ] ; then 
-		echo "Extracting Grass...."
-		tar -zxf grass-6.4.4.tar.gz
-		cd grass-6.4.4
+	grass64 --version >& /dev/null
 
-		echo "Configuring Grass...."
-		echo "Be patient. Grass is a massive toolset. This will take a couple of minutes."
-		./configure --prefix=$HOME --with-proj-includes=$HOME/include --with-proj-libs=$HOME/lib \
-			--with-proj-share=$HOME/share/proj --with-fftw-libs=$HOME/lib \
-			--with-fftw-includes=$HOME/include > /dev/null
-		make > /dev/null
-		
-		echo "Installing Grass to ~/bin...."
-		make install > /dev/null
+	if [ $? -ne 0 ] ; then 
+		echo "Installing Grass...." 
+
 		cd $HOME
+		wget http://grass.osgeo.org/grass64/source/grass-6.4.4.tar.gz
+	
+		if [ $? -eq 0 ] ; then 
+			echo "Extracting Grass...."
+			tar -zxf grass-6.4.4.tar.gz
+			cd grass-6.4.4
 
-		echo "Finished installing Grass to ~/bin."
+			echo "Configuring Grass...."
+			echo "Be patient. Grass is a massive toolset. This will take a couple of minutes."
+			./configure --prefix=$HOME --with-proj-includes=$HOME/include --with-proj-libs=$HOME/lib \
+				--with-proj-share=$HOME/share/proj --with-fftw-libs=$HOME/lib \
+				--with-fftw-includes=$HOME/include > /dev/null
+			make > /dev/null
+		
+			echo "Installing Grass to ~/bin...."
+			make install > /dev/null
+			cd $HOME
+
+			echo "Finished installing Grass to ~/bin."
+
+		else
+			echo "Unable to download Grass from OSGeo."
+		fi
 
 	else
-		echo "Unable to download Grass from OSGeo."
+		echo "Grass detected."
 	fi
 
 else
@@ -234,5 +261,5 @@ echo "Updating path variable."
 
 echo "export PATH=$PATH:~/bin:~/cctools/bin" >> .bashrc
 
-echo -n "New path is : " 
-echo $PATH
+echo "Please open a new terminal for the updated path to take effect." 
+echo ""
