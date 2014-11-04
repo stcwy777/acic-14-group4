0fe2a2058ce053006aba4b5e2f3192e347d8ddc0
Brayden Streeter <bstreete@email.arizona.edu>
2014-11-03 18:35:10 -0700

    Updated read_meta to extract all of the archives. Combine all of the separated TIF files. Then warp all of the files to Daymet's projection. After each step, old files are deleted to save on space. This also prevents redoing work. If the code doesn't finish, the appropriate set of files will still remain to be processed.

--- read_meta.py
+++ read_meta.py
@@ -5,12 +5,9 @@ import sys
 import decimal
 import ConfigParser
 
-
-def read_meta():
+def driver(): 
 	"""
-	Opens up any metadata*.txt files in the local directory or specified directory if there is one.
-	It will search the files for the EPSG code defining the projection as well as the current zone.
-	This data is saved in a dictionary named coords that is passed to the next functions.
+	Handles the execution of the program. Changes the working directory, un-archives all of the files in the directory, merges the partitioned TIFs into a single master TIF for each dataset, then converts the TIFs from their current projections to the projection defined by Daymet. 
 	"""
 
 	# Change to passed directory, if applicable
@@ -28,6 +25,56 @@ def read_meta():
 		print "Usage: %s [directory]" % sys.argv[0]
 		sys.exit(1)
 
+	# Untar all of the files in the working directory before merging 
+	# Then remove the old archives. 
+
+	print '\nExtracting archived DEMs from Open Topography....\n'
+	path = os.path.join(os.getcwd(), '*.tar.gz')
+	extract_dems(glob(path))
+	
+	# Need to merge subfiles for each DEM output into a single TIFF
+	# And remove the now unneeded partial DEMs
+
+	print 'Merging tiled DEMs...\n'
+
+	# D Infinity Catchment Area
+	print 'Merging catchment DEMs....'
+	path = os.path.join(os.getcwd(), "scap*.tif")
+	merge_files(glob(path), 'catch_total.tif')
+
+	print 'Merging flow DEMs....'
+	# D Infinity Flow 
+	path = os.path.join(os.getcwd(), "angp*.tif")
+	merge_files(glob(path), 'flow_total.tif')
+
+	print 'Merging slope DEMs....'
+	# D Infinity Slope
+	path = os.path.join(os.getcwd(), "slpp*.tif")
+	merge_files(glob(path), 'slope_total.tif')
+
+	print 'Merging pit remove DEMs....'
+	# Pit Remove 
+	path = os.path.join(os.getcwd(), 'felp*.tif')
+	merge_files(glob(path), 'pit_total.tif')
+
+	print 'Merging TWI DEMs....'
+	# Total Wetness Index
+	path = os.path.join(os.getcwd(), 'twip*.tif')
+	merge_files(glob(path), 'twi_total.tif')
+	
+	# Read the metadata to determine what projection to warp 
+	# After reading, it will pass the projection info to convert_opentopo 
+	# To update all of the available TIF files
+	read_meta() 
+
+
+def read_meta():
+	"""
+	Opens up any metadata*.txt files in the local directory or specified directory if there is one.
+	It will search the files for the EPSG code defining the projection as well as the current zone.
+	This data is saved in a dictionary named coords that is passed to the next functions.
+	"""
+
 	# Try opening the file and searching
 	try:
 		path = os.path.join(os.getcwd(), "metadata*.txt")
@@ -42,7 +89,6 @@ def read_meta():
 					if line.startswith("Horizontal Coordinates:"):
 						proj_info['region'] = line[-8:-3]
 						proj_info['zone'] = proj_info['region'][-2:] + 'n'
-						print proj_info
 	except IOError:
 		print 'Unable to open file.'
 		sys.exit(1)
@@ -54,7 +100,10 @@ def read_meta():
 
 	else:
 		# Convert the DEMs to Daymet's projection
+		print 'Converting OpenTopography DEMs to Daymet\'s projection.\n'
 		coords = convert_opentopo(proj_info)
+
+		print 'Finished warping OpenTopography.' 
 
 # End read_meta()
 
@@ -72,78 +121,97 @@ def convert_opentopo(proj_info):
 	"""
 
 	# Command string to convert the DEM files from Open Topography to DAYMET's projection
-	command = ['gdalwarp', '-s_srs', 'EPSG:' + proj_info['region'], '-t_srs',
+	command = ['gdalwarp', '-s_srs', 'EPSG:' + proj_info['region'], '-overwrite', '-t_srs',
 			   "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs",
 			   '-r', 'bilinear', '-of', 'GTiff']
 
-	# Need to merge subfiles for each DEM output into a single TIFF
-
-	# D Infinity Catchment Area
-	path = os.path.join(os.getcwd(), "scap*.tif")
-	merge_files(glob(path), 'scap_total.tif')
-
-	# D Infinity Flow 
-	path = os.path.join(os.getcwd(), "angp*.tif")
-	merge_files(glob(path), 'angp_total.tif')
-
-	# D Infinity Slope
-	path = os.path.join(os.getcwd(), "slpp*.tif")
-	merge_files(glob(path), 'slpp_total.tif')
-
-	# Pit Remove 
-	path = os.path.join(os.getcwd(), 'felp*.tif')
-	merge_files(glob(path), 'felp_total.tif')
-
-	# Total Wetness Index
-	path = os.path.join(os.getcwd(), 'twi*.tif')
-	merge_files(glob(path), 'twi_total.tif')
-
-	# D Infinity 
-
-	# Need to execute for each total .tif file from OpenTopo
-	path = os.path.join(os.getcwd(), "*total.tif")
+	# Need to execute for each .tif file from OpenTopo
+	path = os.path.join(os.getcwd(), "*.tif")
 
 	for dem_file in glob(path):
-		# Create the output file name
-		dem_output = dem_file[:-4] + '.converted.tif'
 
-		print "Creating %s" % dem_output
+		# Check that the file is not already warped
+		if path.find('converted.tif') < 0:
+			# Create the output file name
+			dem_output = dem_file[:-4] + '.converted.tif'
 
-		# Add the filenames to the end of the list
-		command.append(dem_file)
-		command.append(dem_output)
+			print "Creating %s" % dem_output
 
-		# Execute the gdalwarp command
-		process = Popen(command, stdout=PIPE, shell=False)
+			# Add the filenames to the end of the list
+			command.append(dem_file)
+			command.append(dem_output)
 
-		# Check for errors
-		stdout, stderr = process.communicate()
+			# Execute the gdalwarp command
+			process = Popen(command, stdout=PIPE, shell=False)
 
-		if stderr is not None:
-			print stderr
-			sys.exit(1)
+			# Check for errors
+			stdout, stderr = process.communicate()
 
-		# Remove the filenames for next iteration
-		command.remove(dem_file)
-		command.remove(dem_output)
+			if stderr is not None:
+				print stderr
 
-# End convert_opentopo() 57 - 68
+			# Remove the original file
+			else:
+				os.remove(dem_file)
+				print 'Successfully created %s' % dem_output
+
+			# Remove the filenames for next iteration
+			command.remove(dem_file)
+			command.remove(dem_output)
+
+# End convert_opentopo()
 	
 def merge_files(path, output): 
 	"""
-	Merges the filenames passed into a single TIFF file with gdalwarp. Assumes that the system running the application has at least 2 GB of available memory.
+	Merges the filenames passed into a single TIFF file with gdalwarp. Assumes that the system running the application has at least 2 GB of available memory. Deletes the individual chunks of the file after creating the single combined TIFF. 
 	"""
 
 	# Create the command to execute
-	command = ['gdalwarp', '--config', 'GDAL_CACHEMAX', '2000', '-wm', '2000'] 
+	command = ['gdalwarp', '--config', 'GDAL_CACHEMAX', '2000', '-wm', '2000', '-overwrite'] 
 	command.extend(path)
 	command.append(output)
-
-	print command 
 
 	# Execute the command
 	process = Popen(command, stdout=PIPE, shell=False)
 	stdout, stderr = process.communicate()
 
+	if stderr is None: 
+		# Remove the partitioned files
+		for part in path: 
+			os.remove(part)
+
+	else:
+		# Display errors, don't delete
+		print stderr
+		
 # End merge_files()
-read_meta()
+
+def extract_dems(path):
+	"""
+	Extracts all files ending with .tar.gz in the currrent directory. After a successful execution, delete the extracted archive. 
+	"""
+	
+	for archive in path:
+
+		# Setup command for each file
+		command = ['tar', 'zxf'] 
+		command.append(archive)
+
+		process = Popen (command, stdout=PIPE, shell=False)
+		stdout, stderr = process.communicate()
+
+		if stderr is not None: 
+			print '\nErrors encountered extracting %s.\n' % archive
+			print stderr
+
+		# Successfully untarred/ungzipped specified archive
+		# Remove the file
+		else:
+			os.remove(archive)
+			print '\nFinished extracting contents of %s. Archive deleted. \n' % archive
+		# Remove the filename for next iteration
+		command.remove(archive)
+
+# End extract_dems()
+
+driver()
