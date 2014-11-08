ecc4ac6a207ce3d9b93dffa2f6458521b41fb35c
Ubuntu <ubuntu@yunw-001.novalocal>
2014-11-08 22:40:43 +0000

    eemt updated

--- eemt.sh
+++ eemt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g.proj -c proj4="+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
-loop head
+#g.proj -c proj4="+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+#loop head
 #input
 r.in.gdal input=$1 output=dem_10m
 r.in.gdal input=$2 output=tmin
@@ -12,16 +12,15 @@ r.in.gdal input=$6 output=dem_1km
 #run model
 r.slope.aspect elevation=dem_10m slope=slope aspect=aspect
 r.sun -s elevin=dem_10m aspin=aspect slopein=slope day="1" step="0.05" dist="1" insol_time=hours_sun glob_rad=total_sun
-r.mapcalc "zeros=if(dem>0,0,null())"
-r.sun elevin=dem aspin=zeros slopein=zeros day="1" step="0.05" dist="1" glob_rad=flat_total_sun  
+r.mapcalc "zeros=if(dem_10m>0,0,null())"
+r.sun elevin=dem_10m aspin=zeros slopein=zeros day="1" step="0.05" dist="1" glob_rad=flat_total_sun  
 r.mapcalc "S_i=total_sun/flat_total_sun"
 r.mapcalc "a_i=twi/((max(twi)+min(twi))/2)"
-r.mapcalc "F=a_i*prcp"
 r.mapcalc "c_w=4185.5"
 r.mapcalc "NPP=0"
 r.mapcalc "h_bio=22*10^6"
 #loop over days on temp
-for((i=1;i<=365;i++))
+for((t=1;t<=365;t++))
 do
 eemt_tif="${t}_eemt.tif"
 r.mapcalc "tmin_loc_${t}=tmin.${t}-0.00649*(dem_10m-dem_1km)"
@@ -40,8 +39,10 @@ r.mapcalc "tmax_topo_${t}=tmax_loc_${t}*(S_i-(1/S_i))"
 #r.mapcalc "PET=total_sun_joules+p_a*0.001013*(vp_s_topo-vp_loc)/ra))/(2.45*(m_vp+g_psy)"
 #r.mapcalc "AET=prcp*(1+PET/prcp(1(PET/prcp)2.63)(1/2.63))"
 r.mapcalc "DT=((tmax_topo_${t}+tmin_topo_${t})/2)-273.15"
+r.mapcalc "F=a_i*prcp.${t}"
 r.mapcalc "EEMT=F*c_w*DT+NPP*h_bio"
 #output
 g.region rast=EEMT
-r.out.gdal-c createopt="TFW=YES,COMPRESS=LZW" input=EEMT output=$eemt_tif
+r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=EEMT output=$eemt_tif
 done
+#g.mremove "*"
