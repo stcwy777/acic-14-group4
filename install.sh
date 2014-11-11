cfd659eac59e494f1dd8dd5ca00b1970d696f1e2
Brayden Streeter <bstreete@email.arizona.edu>
2014-11-11 09:07:29 -0700

    Updated the install script to install most of the requirements on an Ubuntu machine with root privileges. The user has to handle adding lines to the path and to the apt-get sources list.

--- install.sh
+++ install.sh
@@ -1,266 +1,63 @@
 #!/bin/bash
 
-# Check for CC Tools
-~/cctools/bin/work_queue_status -v &> /dev/null
-
-if [ $? -ne 0 ] ; then 
-
-	work_queue_status -v &> /dev/null 
-	
-	if [ $? -ne 0 ] ; then 
-		echo "Installing CCTools..."
-
-		# Download the source files
-		cd 
-		wget "http://ccl.cse.nd.edu/software/files/cctools-4.2.2-source.tar.gz"
-
-		# If the download completed successfully
-		if [ $? -eq 0 ] ; then
-		
-			# Extract
-			tar xzf cctools-4.2.2-source.tar.gz
-			cd cctools-4.2.2-source
-
-			echo "Configuring CCTools...."
-			./configure > /dev/null
-			make > /dev/null
-
-			echo "Installing to ~/cctools/bin...."
-			make install > /dev/null
-
-			cd 
-			echo "Finished installing CCTools ~/cctools/bin."
-
-		else 
-			echo "Failed to download and install CCTools from Notre Dame." 
-		fi
-	
-	else
-		echo "CCTools detected."
-	fi
-
-else
-	echo "CCTools detected."
-fi
-
-# Check for gdal packages
-~/bin/gdal-config --version &> /dev/null
-
-if [ $? -ne 0 ] ; then
-
-	gdal-config --version &> /dev/null 
-	
-	if [ $? -ne 0 ] ; then 
-
-		echo "Installing GDAL...."
-
-		# Download source files
-		cd 
-		wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz
-
-		if [ $? -eq 0 ] ; then
-			# Extract files
-			tar -zxf gdal-1.11.1.tar.gz
-			cd gdal-1.11.1
-
-			echo "Configuring GDAL...."
-			echo "Be patient, this takes a couple of minutes."
-			./configure --prefix=$HOME --with-python > /dev/null
-			make > /dev/null
-
-			echo "Installing GDAL in ~/bin...."
-			make install > /dev/null
-			cd $HOME
-
-			echo "Finished installing GDAL to ~/bin."
-
-		else
-			echo "Failed to download GDAL from OSGeo.org."
-		fi
-	
-	else
-		echo "GDAL detected."
-	fi
-
-else
-	echo "GDAL detected." 
-fi
-
-# iCommands
-~/icommands/ils -v &> /dev/null
-
-if [ $? -ne 0 ] ; then 
-
-	ils -v &> /dev/null
-	if [ $? -ne 0 ] ; then
-
-		echo "Installing iCommands...."
-
-		cd $HOME
-		wget http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2
-	
-		if [ $? -eq 0 ] ; then
-			echo "Extracting iCommands...."
-			tar -jxf icommands.x86_64.tar.bz2
-
-			echo "Finished installing iCommands to ~/icommands."
-			cd $HOME
-
-		else
-			echo "Unable to download iCommands from iPlant."
-		fi
-
-	else
-		echo "iCommands detected."
-	fi
-
-else
-	echo "iCommands detected."
-fi
-
-# Check for FFTW
-test -f ~/lib/libfftw3.la &> /dev/null
-# Installs into ~/bin and ~/include
-if [ $? -ne 0 ] ; then 
-
-	echo "Installing FFTW...."
-	cd $HOME
-	wget http://www.fftw.org/fftw-3.3.4.tar.gz
-	
-	if [ $? -eq 0 ] ; then 
-		echo "Extracting FFTW...."
-		tar -zxf fftw-3.3.4.tar.gz
-
-		cd fftw-3.3.4
-
-		echo "Configuring FFTW...."
-		./configure --prefix=$HOME > /dev/null
-
-		echo "Installing FFTW to ~/bin...."
-		make > /dev/null
-		make install > /dev/null
-		cd $HOME
-
-		echo "Finished installing FFTW."
-
-	else
-		echo "Unable to download FFTW from FFTW.org."
-	fi
-
-else
-	echo "FFTW detected."
-fi
-
-# Check for Proj.4
-test -f ~/bin/proj &> /dev/null
-# # Installs into ~/bin, ~/include, and ~/share/proj
-
-if [ $? -ne 0 ] ; then
-	echo "Installing Proj-4...."
-	cd $HOME 
-	wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz
-	
-	if [ $? -eq 0 ] ; then 
-
-		echo "Extracting Proj-4...."
-		tar -zxf proj-4.8.0.tar.gz
-		cd proj-4.8.0
-
-		echo "Configuring Proj-4...."
-		./configure --prefix=$HOME > /dev/null
-		make > /dev/null
-
-		echo "Installing Proj-4 to ~/bin...."
-		make install > /dev/null
-		cd $HOME
-
-		echo "Finished installing Proj-4 to ~/bin."
-
-	else
-		echo "Unable to download Proj-4 from OSGeo."
-	fi
-
-else
-	echo "Proj-4 detected."
-fi
-
-
-# Check for Grass
-# Installs into ~/bin
-~/bin/grass64 --version >& /dev/null
-
-if [ $? -ne 0 ] ; then 
-
-	grass --version >& /dev/null
-
-	if [ $? -ne 0 ] ; then 
-		echo "Installing Grass...." 
-
-		cd $HOME
-		wget http://grass.osgeo.org/grass64/source/grass-6.4.4.tar.gz
-	
-		if [ $? -eq 0 ] ; then 
-			echo "Extracting Grass...."
-			tar -zxf grass-6.4.4.tar.gz
-			cd grass-6.4.4
-
-			echo "Configuring Grass...."
-			echo "Be patient. Grass is a massive toolset. This will take a couple of minutes."
-			./configure --prefix=$HOME --with-proj-includes=$HOME/include --with-proj-libs=$HOME/lib \
-				--with-proj-share=$HOME/share/proj --with-fftw-libs=$HOME/lib \
-				--with-fftw-includes=$HOME/include > /dev/null
-			make > /dev/null
-		
-			echo "Installing Grass to ~/bin...."
-			make install > /dev/null
-			cd $HOME
-
-			echo "Finished installing Grass to ~/bin."
-
-		else
-			echo "Unable to download Grass from OSGeo."
-		fi
-
-	else
-		echo "Grass detected."
-	fi
-
-else
-	echo "Grass detected." 
-fi
-
-echo "Cleaning up...."
-
-cd $HOME
-
-# CCTools
-rm cctools-4.2.2-source.tar.gz >& /dev/null
-rm -r cctools-4.2.2-source >& /dev/null
-
-# GDAL
-rm gdal-1.11.1.tar.gz >& /dev/null
-rm -fr gdal-1.11.1 >& /dev/null
-
-# iCommands
-rm icommands.x86_64.tar.bz2 >& /dev/null
-
-# Grass
-rm grass-6.4.4.tar.gz >& /dev/null
-
-# FFTW
-rm fftw-3.3.4.tar.gz >& /dev/null
-rm -fr fftw-3.3.4 >& /dev/null
-
-# proj.4
-rm proj-4.8.0.tar.gz >& /dev/null
-rm -fr proj-4.8.0 >& /dev/null
-
-echo "Finished removing unneeded files." 
-
-echo "Updating path variable."
-
-echo "export PATH=$PATH:~/bin:~/cctools/bin" >> .bashrc
-echo "export PYTHONPATH=${PYTHONPATH}:~/cctools/lib/python2.6/site-packages" >> .bashrc
-
-echo "Please open a new terminal for the updated path to take effect." 
-echo ""
+# Assumes the user is on an Ubuntu machine with root permissions.
+# Downloads and installs all of the necessary software for 
+# generating the EEMT model, including the group's code.
+
+# User needs to update the Repo List manually first for R 3.1+
+# Add this line to /etc/apt/sources.list
+# deb http://cran.rstudio.com/bin/linux/ubuntu trusty/
+
+# Pull new updates
+sudo apt-get update -y
+
+# Install Applications
+sudo apt-get install -y grass-dev coop-computing-tools python-pip git
+
+
+# Get R 3.1+ 
+	# Add keys for new repo
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y r-base
+
+# Get DaymetR and install it 
+wget https://bitbucket.org/khufkens/daymetr/get/master.zip
+unzip master.zip
+
+# Get iCommands 
+wget http://www.iplantcollaborative.org/sites/default/files/irods/icommands.x86_64.tar.bz2
+tar jxf icommands.x86_64.tar.bz2
+	# Update PATH
+echo "export PATH=$PATH:~/icommands"
+source .bashrc
+
+# Clean up extra
+cd 
+rm master.zip
+rm icommands.x86_64.tar.bz2
+
+# Download the code and change to directory
+git init
+git clone https://github.com/bstreete/acic-14-group4.git
+
+#Install packages for DaymetR
+cd khufkens-daymetr*
+
+echo "To finish installing DaymetR, follow these instructions:"
+echo
+echo "sudo R"
+echo "install.packages(\"sp\")"
+echo "86"
+echo "install.packages(\"rgeos\")"
+echo "install.packages(\"rgdal\")"
+echo "q()"
+echo "n"
+echo "sudo R CMD INSTALL DaymetR.tar.gz"
+echo
+echo "Add this line to .bashrc for icommands:"
+echo "export PATH=$PATH:~/icommands"
+echo "source .bashrc"
